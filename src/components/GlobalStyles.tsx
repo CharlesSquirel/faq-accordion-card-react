804844b7ff55fc92d7cf2093896fd9a262c74570
Karol Wiewiórka <karol.wiewiorka@gmail.com>
2023-05-12 14:11:49 +0200

    Add color variables

---
+++ src/components/GlobalStyles.tsx
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+:root {
+    --dark-violet: #1d1e35;
+    --orange: #f47c57;
+    --light-violet: #af67e9;
+    --blue: #6565e7;
+    --dark-gray: #4a4b5e;
+    --light-gray: #787887;
+    --very-light-blue: #e7e7e9;
+    --black: #000;
+    --white: #fff;
+}
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+`;
